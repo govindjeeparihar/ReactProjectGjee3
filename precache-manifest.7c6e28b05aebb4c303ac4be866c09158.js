5e9da263214f704bd9d21b24b73846021412090c
GovindJeeParihar <govindam88@gmail.com>
2020-09-29 16:27:42 +0530

    Updates

---
+++ precache-manifest.7c6e28b05aebb4c303ac4be866c09158.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef1673992efbadfda315bfcbe63e4a5",
+    "url": "/ReactProjectGjee3/index.html"
+  },
+  {
+    "revision": "705a019809e099ddaf56",
+    "url": "/ReactProjectGjee3/static/css/main.32ad55fc.chunk.css"
+  },
+  {
+    "revision": "ada65deadb3ef66e195d",
+    "url": "/ReactProjectGjee3/static/js/2.d3b50f9c.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/ReactProjectGjee3/static/js/2.d3b50f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705a019809e099ddaf56",
+    "url": "/ReactProjectGjee3/static/js/main.5e79d368.chunk.js"
+  },
+  {
+    "revision": "2b7dab610bc984412597",
+    "url": "/ReactProjectGjee3/static/js/runtime-main.91b49d20.js"
+  },
+  {
+    "revision": "bed28ddd2a204b473511e6a79fb510c2",
+    "url": "/ReactProjectGjee3/static/media/about-1.bed28ddd.jpg"
+  },
+  {
+    "revision": "b06cab9ae2ed09c8d3b1298a3e01b897",
+    "url": "/ReactProjectGjee3/static/media/about-2.b06cab9a.jpg"
+  },
+  {
+    "revision": "68aecef03d591e5337df3529fea1e355",
+    "url": "/ReactProjectGjee3/static/media/business-man.68aecef0.png"
+  },
+  {
+    "revision": "2ac050411e533b42ad59eb2a72299ae5",
+    "url": "/ReactProjectGjee3/static/media/slider-1.2ac05041.jpg"
+  },
+  {
+    "revision": "7c44b60f72ea68d7341358061e9c25c8",
+    "url": "/ReactProjectGjee3/static/media/slider-2.7c44b60f.jpg"
+  },
+  {
+    "revision": "b8db7a09006683eae5b5174b6efce5f1",
+    "url": "/ReactProjectGjee3/static/media/slider-3.b8db7a09.jpg"
+  }
+]);
